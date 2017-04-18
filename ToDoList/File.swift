9a8032a150abc7a5640bd65b476be7eaa5b70550
Connor Dear <connordear@gmail.com>
2017-04-18 18:06:20 -0400

    Added tableview and built Task Class

---
+++ ToDoList/File.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  ToDoList
+//
+//  Created by Connor Dear on 2017-04-18.
+//  Copyright © 2017 Connor Dear. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = ""
+    var important = false
+}
--- ToDoList/ViewController.swift
+++ ToDoList/ViewController.swift
@@ -8,18 +8,52 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var taskList : [Task] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        taskList = buildTaskList()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return taskList.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let task = taskList[indexPath.row]
+        if task.important {
+            cell.textLabel?.text = "‼️\(task.name)"
+        } else {
+            cell.textLabel?.text = task.name
+        }
+        return cell
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func buildTaskList() -> [Task] {
+        let task1 = Task()
+        task1.name = "Go grocery Shopping"
+        task1.important = true
+        
+        let task2 = Task()
+        task2.name = "Hold David's Hand"
+        task2.important = true
+        
+        let task3 = Task()
+        task3.name = "Finish swift tutorial"
+        task3.important = false
+        
+        let tasks = [task1, task2, task3]
+        
+        return tasks
     }
-
 
 }
-
